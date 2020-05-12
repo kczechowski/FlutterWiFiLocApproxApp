1c89cc302a272937f610466e752765647cdaf453
kczechowski <karolcz97@gmail.com>
2020-05-12 18:40:15 +0200

    Added WifiFinderService as dependency to WifiLocService

--- lib/services/wifi_loc_service.dart
+++ lib/services/wifi_loc_service.dart
@@ -2,16 +2,23 @@ import 'package:kczwifilocation/api/models/network.dart';
 import 'package:kczwifilocation/api/models/network_filter.dart';
 import 'package:kczwifilocation/api/wifi_loc_api.dart';
 import 'package:http/http.dart' as http;
+import 'package:kczwifilocation/services/wifi_finder_service.dart';
 
 class WifiLocService {
 
   WifiLocAPI wifiLocAPI;
+  WifiFinderService wifiFinderService;
 
-  WifiLocService({WifiLocAPI api}) {
+  WifiLocService({WifiLocAPI api, WifiFinderService wifiFinderService}) {
     if(api != null)
       this.wifiLocAPI = api;
     else {
       this.wifiLocAPI = WifiLocAPI(httpClient: http.Client());
+    }
+    if(wifiFinderService != null)
+      this.wifiFinderService = wifiFinderService;
+    else {
+      this.wifiFinderService = WifiFinderService();
     }
   }
 
